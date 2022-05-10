9be4fe46ccb923bd5f9630b379f8cc8368171d75
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2022-05-10 11:09:24 -0400

    Initial Commit

---
+++ Photo Sort/Photo_SortApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_SortApp.swift
+//  Photo Sort
+//
+//  Created by Michael Horowitz on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Photo_SortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
